67fcf82f5150821a0220c786cd7756e5735b3607
미현 <douob0@naver.com>
2020-08-29 23:28:00 +0900

    🐛 fix : Modify order form to access session

--- fc_django/order/forms.py
+++ fc_django/order/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import Order
+from product.models import Product
+from fcuser.models import Fcuser
 
 
 class RegisterForm(forms.Form):
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = request
+
     quantity = forms.IntegerField(
         error_messages={
             'required' : '수량을 입력해주세요.'
@@ -18,3 +24,17 @@ class RegisterForm(forms.Form):
         cleaned_data = super().clean()
         quantity = cleaned_data.get('quantity')
         product = cleaned_data.get('product')
+        fcuser = self.request.session.get('user')
+
+        if quantity and product and fcuser:
+            order = Order(
+                quantity = quantity,
+                product = Product.objects.get(pk=product),
+                fcuser = Fcuser.objects.get(email=fcuser)
+            )
+            order.save()
+        else:
+            self.product = product
+            self.add_error('quantity', '값이 없습니다')
+            self.add_error('product', '값이 없습니다')
+        

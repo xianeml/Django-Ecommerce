29cc299807a726f4f12a0046a5fd82fdb9e86bd0
미현 <douob0@naver.com>
2020-08-31 16:42:09 +0900

    🐛 fix : Add login access decorator

--- fc_django/product/views.py
+++ fc_django/product/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
+from django.utils.decorators import method_decorator
+from fcuser.decorators import admin_required
 from .models import Product
 from .forms import RegisterForm
 from order.forms import RegisterForm as OrderForm
@@ -12,6 +14,7 @@ class ProductList(ListView):
     template_name = 'product.html'
     context_object_name = 'product_list'
 
+@method_decorator(admin_required, name='dispatch')
 class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm
